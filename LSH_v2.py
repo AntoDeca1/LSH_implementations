e1f9bb5772f4f20134a8e57cd595f89f069916b8
Antonio De Candia <antodeca@Macbook-Pro-di-Antodeca.local>
2024-04-22 10:45:29 +0200

    output similarities_3

--- LSH_v2.py
+++ LSH_v2.py
@@ -5,13 +5,6 @@ from collections import defaultdict
 import time
 
 
-# RIMUOVERE TUTTI QUESTI TRANSPOSE AGENDO SULLA FUNZIONE PROJECT MATRIX
-
-# DA OTTIMIZZARE
-# Potrebbe essere da ottimizzare come calcolo la cosine similairity.
-# IDEA:Potrei prendere i candidati da tutti i bucket di livello 1 e continuare a prendere tutti i candidati fin quando non ne ho almeno k
-# Trovare una maniera di calcolare le similarità in maniera matriciale magari sfruttando le matrici di scipy che sembrano velocizzare il calcolo
-
 class RandomProjections():
     def __init__(self, d, nbits, l=1, seed=42):
         """
@@ -36,21 +29,26 @@ class RandomProjections():
         For each vector returns the list of its k candidates
         :return: matrix containing the candidates for each vector in the index
         """
+        num_items = self.buckets_matrix.shape[0]
+        candidates_matrix = np.zeros((num_items, k)).astype(int)
         for index, row in enumerate(self.buckets_matrix):
             candidates = self.search(row, index, k)
-            if index == 0:
-                candidates_matrix = candidates
-            else:
-                candidates_matrix = np.vstack([candidates_matrix, candidates])
-        print()
+            candidates_matrix[index, :] = candidates
         return candidates_matrix
 
     def output_similarities(self, k=5):
         """
         Designed to test LSH using Elliot in a simple way
+        QUA POSSO PROVARE CON IL MAPPING SENZA LA CANDIDATES MATRIX
+        Se qui uso un mapping con un vettore associato  posso risolvere forse in maniera intelligente
+        Devo solo bloccare dopo i primi k elementi
+        Provare a migliorare questo in termini di efficienza con una unica operazione matriciale
         :return:
         """
+        prima = time.time()
         candidates = self.candidates_matrix(k)
+        dopo = time.time()
+        print(dopo - prima, "Candidates Matrix")
 
         index = self._input_matrix.toarray()
 
@@ -60,13 +58,103 @@ class RandomProjections():
         num_items = candidates.shape[0]
 
         # Initialize the matrix to hold cosine similarities
-        similarity_matrix = np.zeros((num_items, k))
+        similarity_matrix = np.empty((num_items, k))
         # Iniziamo con un for
 
         for i, vector in enumerate(self._input_matrix):
+            prima = time.time()
             similarity_matrix[i, :] = cosine_similarity(vector, candidates_vectors[i])
+            dopo = time.time()
+            print(dopo - prima, "For loop output_similarities")
 
-        return similarity_matrix
+        return similarity_matrix, candidates
+
+    def output_similarities_3(self, k=5):
+        prima = time.time()
+        candidates = self.candidates_list(k)  # This should return a list of candidate indices for each item
+        dopo = time.time()
+        print(dopo - prima, "Candidates Mapping")
+
+        # Flatten the list of candidate indices to fetch all vectors at once
+        all_candidate_indices = np.concatenate(candidates)
+        all_candidate_vectors = self._input_matrix[all_candidate_indices]
+
+        # Now create an array to map back each candidate to its original item
+        item_index = np.repeat(np.arange(len(candidates)), [len(c) for c in candidates])
+
+        # Compute cosine similarities in batch
+        prima = time.time()
+        all_similarities = cosine_similarity(self._input_matrix, all_candidate_vectors)
+        dopo = time.time()
+        print(dopo - prima, "Cosine similarity batch computation")
+
+        # Sort and select the top k for each item
+        similarity_matrix = []
+        start_index = 0
+        for i, size in enumerate([len(c) for c in candidates]):
+            end_index = start_index + size
+            item_similarities = all_similarities[i, start_index:end_index]
+            sorted_indices = np.argsort(-item_similarities)[:k]
+            top_k_similarities = item_similarities[sorted_indices]
+            similarity_matrix.append(top_k_similarities)
+            start_index = end_index
+
+        similarity_matrix = np.array(similarity_matrix)
+
+        return similarity_matrix, candidates
+
+    def output_similarities_2(self, k=5):
+        """
+        Designed to test LSH using Elliot in a simple way
+        QUA POSSO PROVARE CON IL MAPPING SENZA LA CANDIDATES MATRIX
+        Se qui uso un mapping con un vettore associato  posso risolvere forse in maniera intelligente
+        Devo solo bloccare dopo i primi k elementi
+        Provare a migliorare questo in termini di efficienza con una unica operazione matriciale
+        :return:
+        """
+        prima = time.time()
+        candidates = self.candidates_mapping(k)
+        dopo = time.time()
+        print(dopo - prima, "Candidates Mapping")
+
+        # Get number of items
+        data, row_indices = [], []
+
+        for i, vector in enumerate(self._input_matrix):
+            prima = time.time()
+            candidates_vectors = self._input_matrix[candidates[i]]
+            dopo = time.time()
+            print(dopo - prima, "For loop output_similarities_2")
+            c_similarities = cosine_similarity(vector, candidates_vectors).flatten()
+            # sorted_indices = np.argsort(-c_similarities)[:k].flatten()
+            # data.extend(c_similarities[sorted_indices])
+            # row_indices.extend(sorted_indices)
+
+        return data, row_indices
+
+    def candidates_list(self, k):
+        """
+                For each vector returns the list of its k candidates
+                :return: matrix containing the candidates for each vector in the index
+                """
+        candidates_mapping = []
+        for index, row in enumerate(self.buckets_matrix):
+            candidates = self.search_1(row, index, k)
+            candidates_mapping.append(candidates)
+
+        return candidates_mapping
+
+    def candidates_mapping(self, k):
+        """
+                For each vector returns the list of its k candidates
+                :return: matrix containing the candidates for each vector in the index
+                """
+        candidates_mapping = {}
+        for index, row in enumerate(self.buckets_matrix):
+            candidates = self.search_1(row, index, k)
+            candidates_mapping[index] = candidates
+
+        return candidates_mapping
 
     def add(self, input_matrix):
         """
@@ -74,17 +162,34 @@ class RandomProjections():
         We have to consider that we could have also a forest of hyperplanes and so each vector will be inserted in l buckets
         1)  self.buckets_matrix : Matrix having as row the bucket relating to the corresponding vector
         2)  self.mapping: an object containing the mappings between buckets and contained vectors
-        3)  self.uniques_matrix : Contains only the ids of buckets with content and not all the possible 2^nbits buckets id
+        3)  self.all_hashes : Contains only the ids of buckets with content and not all the possible 2^nbits buckets id
+        4)  self.closest_buckets: Pre-Computed closest buckets for each bucket in term of hamming distance(SLOW THE INDEX BUT MAKES THE SEARCH FASTER)
         :param input_matrix: Matrix having as rows the vectors we want to bucketize
         """
-        # centered_matrix = self.center_ratings_matrix(input_matrix)
         centered_matrix = input_matrix
         self._input_matrix = centered_matrix
         buckets = self.project_matrix(centered_matrix)
         self.buckets_matrix = (buckets > 0).astype(int)
         self.all_hashes = np.unique(self.buckets_matrix.reshape(-1, self.nbits), axis=0)
         self.mapping_ = self.create_mappings()
-        print()
+        self.closest_buckets = self.precompute_closest_buckets()  # Da verificare se ne vale la pena precalcolarle
+
+    def precompute_closest_buckets(self):
+        # Dictionary to store the closest buckets for each hash
+        closest_buckets = {}
+        num_hashes = self.all_hashes.shape[0]
+
+        # Compute the Hamming distance between each pair of hashes
+        expanded_hashes = np.expand_dims(self.all_hashes, 1)
+        distances = np.count_nonzero(expanded_hashes != expanded_hashes.transpose(1, 0, 2), axis=2)
+
+        # For each hash, find and store indices of the closest buckets
+        for idx in range(num_hashes):
+            sorted_indices = np.argsort(distances[idx])
+            # Save the closest buckets' indices, skip the first one as it is the hash itself
+            closest_buckets[stringify_array(self.all_hashes[idx])] = sorted_indices  # Adjust slicing as needed
+
+        return closest_buckets
 
     def create_mappings(self):
         """
@@ -116,35 +221,32 @@ class RandomProjections():
                 output = np.concatenate([output, temp])
         return output.transpose(1, 0, 2)
 
-    def search_v2(self, hashed_vec, index, k=5):
+    def search_1(self, hashed_vec, index, k=5):
         """
-        My approach that is in my opinion more efficient in term of reduction of false positives.
-        1) For each vector i pick all the element in the closest buckets in all the l tables
-        2) If i have not reached k elements yet i repeat the first step another time checking for the elements in the second's closest buckets
-        3) Once reached at least k elements i compute the similarity returning the k elements with the highest similarity between the candidates
-        TODO: Optimize the code if necessary
-        TODO: Remove the index as parameter
+        First version.
+        Supponiamo che search vada bene
+        Source:https://github.com/pinecone-io/examples/blob/master/learn/search/faiss-ebook/locality-sensitive-hashing-random-projection/random_projection.ipynb
+        1) I keep taking from the buckets until i reach k elements
+        2) I return the first k without caring of the actual similarity between the query and the candidates
+        For sure this approach is more efficient but in my opinion could decrease too much the performance
+        Optimize the removal of indexes
         :return:
         """
         candidates = set()
         i = 0
+        # Expand A to compare against every row of B
+
         while True:
             for table_id, el in enumerate(hashed_vec):
-                hamming_dist = self.hamming(el, self.all_hashes)
-                stringified_id = stringify_array(hamming_dist[i, :])
-                candidates = candidates | set(self.mapping_[table_id][stringified_id])
+                id = stringify_array(self.all_hashes[self.closest_buckets[stringify_array(el)][i]])
+                candidates = candidates | set(self.mapping_[table_id][id])
             if i == 0:
                 candidates.remove(index)
             if len(candidates) >= k:
                 break
             else:
                 i = i + 1
-        query_vec = self._input_matrix.toarray()[index, :]
-        candidates_vectors = self._input_matrix.toarray()[list(candidates)]
-        cosine_similarities = cosine_similarity(query_vec.reshape(1, -1), candidates_vectors)
-        ordered_indices = np.argsort(cosine_similarities)
-        ordered_candidates = np.array(list(candidates))[ordered_indices].squeeze()
-        return ordered_candidates[-k:]
+        return list(candidates)
 
     def search(self, hashed_vec, index, k=5):
         """
@@ -159,11 +261,12 @@ class RandomProjections():
         """
         candidates = set()
         i = 0
+        # Expand A to compare against every row of B
+
         while True:
             for table_id, el in enumerate(hashed_vec):
-                hamming_dist = self.hamming(el, self.all_hashes)
-                stringified_id = stringify_array(hamming_dist[i, :])
-                candidates = candidates | set(self.mapping_[table_id][stringified_id])
+                id = stringify_array(self.all_hashes[self.closest_buckets[stringify_array(el)][i]])
+                candidates = candidates | set(self.mapping_[table_id][id])
             if i == 0:
                 candidates.remove(index)
             if len(candidates) >= k:
@@ -171,17 +274,6 @@ class RandomProjections():
             else:
                 i = i + 1
         return list(candidates)[:k]
-
-    def center_ratings_matrix(self, items_matrix):
-        """
-        NOT NECESSARY PROBABLY !!!!!
-        If i apply RandomProjections with Explicit ratings all the vectors will be on the positive Side of the hyperPlane
-        My Solution: Remove the mean for each row!!!!
-        :return:
-        """
-        means = np.mean(items_matrix, axis=1).reshape(-1, 1)
-        centered_matrix = items_matrix - means
-        return centered_matrix
 
     def _initialize_projection_matrix(self):
         """
--- main_v2.py
+++ main_v2.py
@@ -2,15 +2,16 @@ import numpy as np
 from LSH_v2 import RandomProjections
 from utils import create_sparse_matrix
 import time
+from sklearn.metrics.pairwise import cosine_similarity
 
 """
 PARAMETERS
 """
 seed = 42
-nbits = 5  # number of hyperplanes in a three
-m = 6040  # number of users
-n = 3675  # number of items
-l = 5  # number of thress in the forest
+nbits = 16  # number of hyperplanes in a three
+m = 6400  # number of users
+n = 3750  # number of items
+l = 2  # number of thress in the forest
 np.random.seed(seed)
 """
 INPUT
@@ -32,11 +33,16 @@ print("Time to index the vectors", end - start)
 Candidates
 """
 # candidates_matrix = rp.candidates_matrix(k=5)
+start = time.time()
+similarities = cosine_similarity(user_item_matrix_dummy.T)
+end = time.time()
+print("Time to compute the itemXitem similarity",end-start)
 "Similarities and Indexes"
 start = time.time()
-prova = rp.output_similarities()
+prova = rp.output_similarities_3()
 end = time.time()
-print("Time to compute the similarity", end - start)
+print("Time to compute the similarity with LSH", end - start)
+
 # print("data", data)
 # print("rows_indicies", rows_indices)
 # print()
